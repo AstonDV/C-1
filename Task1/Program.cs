88039c5cf4a6dcfdcec1e2df0d31063d10f50d74
AstonDV <astondv.kms@gmail.com>
2023-01-23 18:42:21 +1000

    End task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Комментарии
+
+/*влаорылворало
+выапвыа
+авыавыаыв*/
+
+// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+Console.Write("Введи число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+// Решение математически
+int num = number * number;
+Console.WriteLine(num);
+Console.WriteLine($"Квадрат числа {number} равен {num}");
+
+// Решение через библиотеку
+int num2 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} равен {num2}");
