9523c8de1bb1ee6d585caf3f445d03bf4137360c
AstonDV <astondv.kms@gmail.com>
2023-01-23 19:20:05 +1000

    End task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+if (x == 1)
+{
+    Console.Write("Понедельник");
+}
+
+if (x == 2)
+{
+    Console.Write("Вторник");
+}
+
+if (x == 3)
+{
+    Console.Write("Среда");
+}
+
+if (x == 4)
+{
+    Console.Write("Четверг");
+}
+
+if (x == 5)
+{
+    Console.Write("Пятница");
+}
+
+if (x == 6)
+{
+    Console.Write("Суббота");
+}
+
+if (x == 7)
+{
+    Console.Write("Воскресенье");
+}
+
+else if (x < 1 || x > 7)
+{
+    Console.Write("Такого дня недели не бывает");
+}
